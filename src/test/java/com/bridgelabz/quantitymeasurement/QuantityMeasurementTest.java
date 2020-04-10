e7487983dd5daf3f518191021741d0f916a15ff4
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2020-04-10 17:39:12 +0530

    ADDED : Test case 1.2 given two objects, if one of them is null should return not equal

--- src/test/java/com/bridgelabz/quantitymeasurement/QuantityMeasurementTest.java
+++ src/test/java/com/bridgelabz/quantitymeasurement/QuantityMeasurementTest.java
@@ -4,15 +4,23 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class QuantityMeasurementTest {
+    QuantityMeasurement quantityMeasurement = new QuantityMeasurement();
     @Test
     public void givenTwoFeetValues_IfCompared_ShouldReturnAppropriateResult() {
-        QuantityMeasurement quantityMeasurement = new QuantityMeasurement();
-        Feet f1 = new Feet(0);
-        Feet f2 = new Feet(0);
-        Feet f3 = new Feet(5);
+        Feet f1 = new Feet(0.0);
+        Feet f2 = new Feet(0.0);
+        Feet f3 = new Feet(5.0);
         String result1 = quantityMeasurement.compare(f1, f2);
         String result2 = quantityMeasurement.compare(f1, f3);
         Assert.assertEquals("equal", result1);
         Assert.assertEquals("not equal", result2);
+    }
+
+    @Test
+    public void givenTwoFeetObjects_IfOneOfThemIsNull_ShouldReturnNotEqual() {
+        Feet f1 = new Feet(0.0);
+        Feet f2 = null;
+        String result1 = quantityMeasurement.compare(f1, f2);
+        Assert.assertEquals("not equal", result1);
     }
 }
