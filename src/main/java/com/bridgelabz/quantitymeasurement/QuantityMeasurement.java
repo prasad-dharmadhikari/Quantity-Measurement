cde580cbac2e3d3e65ba081dc8dbf602a7c1875f
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2020-04-10 14:45:00 +0530

    ADDED : Welcome message

---
+++ src/main/java/com/bridgelabz/quantitymeasurement/QuantityMeasurement.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.quantitymeasurement;
+
+public class QuantityMeasurement {
+    public static void main(String[] args) {
+        System.out.println("Welcome to quantity measurement problem");
+    }
+}
