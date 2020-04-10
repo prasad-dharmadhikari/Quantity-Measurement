08842cf01e8db854fad226adb3b249afd7bfc42e
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2020-04-10 21:34:01 +0530

    ADDED : Test case 1.5 To check if the values of variables is same or ot

--- src/test/java/com/bridgelabz/quantitymeasurement/QuantityMeasurementTest.java
+++ src/test/java/com/bridgelabz/quantitymeasurement/QuantityMeasurementTest.java
@@ -32,9 +32,17 @@ public class QuantityMeasurementTest {
     }
 
     @Test
-    public void givenTwoObjects_IfTypeIsSame_ShouldReturnAppropriateResult() {
+    public void givenTwoObjects_IfTypeIsSame_ShouldReturnEqual() {
         Feet f1 = new Feet(0.0);
         Feet f2 = new Feet(0.0);
+        String result1 = quantityMeasurement.compare(f1, f2);
+        Assert.assertEquals("equal", result1);
+    }
+
+    @Test
+    public void givenTwoObjects_IfValuesAreSame_ShouldReturnEqual() {
+        Feet f1 = new Feet(12.0);
+        Feet f2 = new Feet(12.0);
         String result1 = quantityMeasurement.compare(f1, f2);
         Assert.assertEquals("equal", result1);
     }
