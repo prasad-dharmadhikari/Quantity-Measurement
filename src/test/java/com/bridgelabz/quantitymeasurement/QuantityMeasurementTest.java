a9772cd9b3bea8886b5ca97e6a7b093f5d205838
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2020-04-11 14:21:38 +0530

    ADDED : Test case 1.6 Given two inch objects if one of them is null, should return not equal

--- src/test/java/com/bridgelabz/quantitymeasurement/QuantityMeasurementTest.java
+++ src/test/java/com/bridgelabz/quantitymeasurement/QuantityMeasurementTest.java
@@ -7,43 +7,62 @@ public class QuantityMeasurementTest {
     QuantityMeasurement quantityMeasurement = new QuantityMeasurement();
     @Test
     public void givenTwoFeetValues_IfCompared_ShouldReturnAppropriateResult() {
-        Feet f1 = new Feet(0.0);
-        Feet f2 = new Feet(0.0);
-        Feet f3 = new Feet(5.0);
-        String result1 = quantityMeasurement.compare(f1, f2);
-        String result2 = quantityMeasurement.compare(f1, f3);
+        Feet firstValue = new Feet(0.0);
+        Feet secondValue = new Feet(0.0);
+        Feet thirdValue = new Feet(5.0);
+        String result1 = quantityMeasurement.compare(firstValue, secondValue);
+        String result2 = quantityMeasurement.compare(firstValue, thirdValue);
         Assert.assertEquals("equal", result1);
         Assert.assertEquals("not equal", result2);
     }
 
     @Test
     public void givenTwoFeetObjects_IfOneOfThemIsNull_ShouldReturnNotEqual() {
-        Feet f1 = new Feet(0.0);
-        Feet f2 = null;
-        String result1 = quantityMeasurement.compare(f1, f2);
+        Feet firstValue = new Feet(0.0);
+        Feet secondValue = null;
+        String result1 = quantityMeasurement.compare(firstValue, secondValue);
         Assert.assertEquals("not equal", result1);
     }
 
     @Test
     public void givenTwoFeetObjects_IfObjectsAreSame_ShouldReturnEqual() {
-        Feet f1 = new Feet(0.0);
-        String result1 = quantityMeasurement.compare(f1, f1);
+        Feet firstValue = new Feet(0.0);
+        String result1 = quantityMeasurement.compare(firstValue, firstValue);
         Assert.assertEquals("equal", result1);
     }
 
     @Test
     public void givenTwoObjects_IfTypeIsSame_ShouldReturnEqual() {
-        Feet f1 = new Feet(0.0);
-        Feet f2 = new Feet(0.0);
-        String result1 = quantityMeasurement.compare(f1, f2);
+        Feet firstValue = new Feet(0.0);
+        Feet secondValue = new Feet(0.0);
+        String result1 = quantityMeasurement.compare(firstValue, secondValue);
         Assert.assertEquals("equal", result1);
     }
 
     @Test
     public void givenTwoObjects_IfValuesAreSame_ShouldReturnEqual() {
-        Feet f1 = new Feet(12.0);
-        Feet f2 = new Feet(12.0);
-        String result1 = quantityMeasurement.compare(f1, f2);
+        Feet firstValue = new Feet(12.0);
+        Feet secondValue = new Feet(12.0);
+        String result1 = quantityMeasurement.compare(firstValue, secondValue);
         Assert.assertEquals("equal", result1);
+    }
+
+    @Test
+    public void givenTwoInchValues_IfCompared_ShouldReturnAppropriateResult() {
+        Inch firstValue = new Inch(0.0);
+        Inch secondValue = new Inch(0.0);
+        Inch thirdValue = new Inch(5.0);
+        String result1 = quantityMeasurement.compare(firstValue, secondValue);
+        String result2 = quantityMeasurement.compare(firstValue, thirdValue);
+        Assert.assertEquals("equal", result1);
+        Assert.assertEquals("not equal", result2);
+    }
+
+    @Test
+    public void givenTwoInchObjects_IfOneOfThemIsNull_ShouldReturnNotEqual() {
+        Inch firstValue = new Inch(0.0);
+        Inch secondValue = null;
+        String result1 = quantityMeasurement.compare(firstValue, secondValue);
+        Assert.assertEquals("not equal", result1);
     }
 }
