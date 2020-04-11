f0cd9833147fe054de205f29c9a2d39160f56c8e
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2020-04-11 23:16:12 +0530

    REFACTORED : Made compare method generic to compare all units of lengths

--- src/main/java/com/bridgelabz/quantitymeasurement/QuantityMeasurement.java
+++ src/main/java/com/bridgelabz/quantitymeasurement/QuantityMeasurement.java
@@ -2,14 +2,8 @@ package com.bridgelabz.quantitymeasurement;
 
 public class QuantityMeasurement {
 
-    public String compare(Feet f1, Feet f2) {
-        if (f1.equals(f2))
-            return "equal";
-        return "not equal";
-    }
-
-    public String compare(Inch i1, Inch i2) {
-        if (i1.equals(i2))
+    public String compare(Distance firstValue, Distance secondValue) {
+        if (firstValue.equals(secondValue))
             return "equal";
         return "not equal";
     }
