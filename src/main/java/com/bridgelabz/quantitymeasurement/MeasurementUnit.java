8d32e2b30a2bf65b1626aaa54d9b8b11e92a44e6
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2020-04-12 15:16:25 +0530

    ADDED : LITRES, GALLON, and MILLILITRES as enum constants

--- src/main/java/com/bridgelabz/quantitymeasurement/MeasurementUnit.java
+++ src/main/java/com/bridgelabz/quantitymeasurement/MeasurementUnit.java
@@ -1,15 +1,15 @@
 package com.bridgelabz.quantitymeasurement;
 
 public enum MeasurementUnit {
-    FEET(12.0), INCH(1.0), YARD(36.0), CENTIMETER(0.4);
-
+    FEET(12.0), INCH(1.0), YARD(36.0), CENTIMETER(0.4),
+    GALLON(3780), LITRES(1000), MILLILITERS(1);
     Double baseValue;
 
     MeasurementUnit(double baseValue) {
         this.baseValue = baseValue;
     }
 
-    public Double convertToBaseValue(Double distance) {
-        return distance * baseValue;
+    public Double convertToBaseValue(Double quantity) {
+        return quantity * baseValue;
     }
 }
