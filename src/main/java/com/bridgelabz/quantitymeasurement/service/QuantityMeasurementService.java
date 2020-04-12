314da027cc5d57f8db19ca42a0fe6903e1ba3309
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2020-04-12 18:30:48 +0530

    REFACTORED : Code according to exceptions

--- src/main/java/com/bridgelabz/quantitymeasurement/service/QuantityMeasurementService.java
+++ src/main/java/com/bridgelabz/quantitymeasurement/service/QuantityMeasurementService.java
@@ -1,14 +1,25 @@
-package com.bridgelabz.quantitymeasurement;
+package com.bridgelabz.quantitymeasurement.service;
 
-public class QuantityMeasurement {
+import com.bridgelabz.quantitymeasurement.exception.QuantityMeasurementException;
+import com.bridgelabz.quantitymeasurement.utility.MeasurementQuantity;
 
-    public String compare(MeasurementQuantity firstValue, MeasurementQuantity secondValue) {
-        if (firstValue.equals(secondValue))
-            return "equal";
-        return "not equal";
+import java.util.Objects;
+
+public class QuantityMeasurementService {
+
+    public String compare(MeasurementQuantity firstValue, MeasurementQuantity secondValue) throws QuantityMeasurementException {
+        if (firstValue == null || secondValue == null)
+            throw new QuantityMeasurementException(QuantityMeasurementException.ExceptionType.NULL_VALUE_ENTERED, "ENTERED NULL VALUE");
+        if (firstValue.type != secondValue.type)
+            throw new QuantityMeasurementException(QuantityMeasurementException.ExceptionType.TYPE_MISMATCH, "TYPE MISMATCH");
+        return !Objects.equals(firstValue, secondValue) ? "not equal" : "equal";
     }
 
-    public Double addTwoValues(MeasurementQuantity firstValue, MeasurementQuantity secondValue) {
+    public Double addTwoValues(MeasurementQuantity firstValue, MeasurementQuantity secondValue) throws QuantityMeasurementException {
+        if (firstValue == null || secondValue == null || firstValue.quantity == null || secondValue.quantity == null)
+            throw new QuantityMeasurementException(QuantityMeasurementException.ExceptionType.NULL_VALUE_ENTERED, "ENTERED NULL VALUE");
+        if (firstValue.type != secondValue.type)
+            throw new QuantityMeasurementException(QuantityMeasurementException.ExceptionType.TYPE_MISMATCH, "Type mismatch");
         return firstValue.quantity + secondValue.quantity;
     }
 }
