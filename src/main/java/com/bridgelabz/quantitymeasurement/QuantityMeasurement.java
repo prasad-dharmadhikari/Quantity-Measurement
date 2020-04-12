6919047e88c3e41215a0e4ae7f301eeadf629f63
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2020-04-12 15:17:20 +0530

    REFACTORED : Changed variable name to quantity to make it more generic

--- src/main/java/com/bridgelabz/quantitymeasurement/QuantityMeasurement.java
+++ src/main/java/com/bridgelabz/quantitymeasurement/QuantityMeasurement.java
@@ -2,13 +2,13 @@ package com.bridgelabz.quantitymeasurement;
 
 public class QuantityMeasurement {
 
-    public String compare(Distance firstValue, Distance secondValue) {
+    public String compare(MeasurementQuantity firstValue, MeasurementQuantity secondValue) {
         if (firstValue.equals(secondValue))
             return "equal";
         return "not equal";
     }
 
-    public Double addTwoValues(Distance firstValue, Distance secondValue) {
-        return firstValue.distance + secondValue.distance;
+    public Double addTwoValues(MeasurementQuantity firstValue, MeasurementQuantity secondValue) {
+        return firstValue.quantity + secondValue.quantity;
     }
 }
