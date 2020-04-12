f4103b81d294bb20e68e3ac4e39887dffd4b89c6
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2020-04-12 14:23:51 +0530

    ADDED : CENTIMETER as Enum constant to compare inch and centimeter

--- src/main/java/com/bridgelabz/quantitymeasurement/MeasurementUnit.java
+++ src/main/java/com/bridgelabz/quantitymeasurement/MeasurementUnit.java
@@ -1,12 +1,14 @@
 package com.bridgelabz.quantitymeasurement;
 
 public enum MeasurementUnit {
-    FEET(12), INCH(1), YARD(36);
+    FEET(12.0), INCH(1.0), YARD(36.0), CENTIMETER(0.4);
+
     Double baseValue;
 
     MeasurementUnit(double baseValue) {
         this.baseValue = baseValue;
     }
+
     public Double convertToBaseValue(Double distance) {
         return distance * baseValue;
     }
